49179f1f36c1a11009d0702e2cfbda0e0e31c35a
apply007 <hasanat153@gmail.com>
2025-11-04 16:22:37 +0600

    ok

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
    baseURL: "http://localhost:8000",
-  // baseURL: "https://api.iconicunityltd.com", // backend root
+  // baseURL: "https://api.iconicunityltd.com", // backend root//
   withCredentials: true, // cookie send হবে
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
